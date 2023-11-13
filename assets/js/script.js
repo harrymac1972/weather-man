fb5081849901849c6e50769c3fa5c88ee55fb27b
harry <harrymac1972@gmail.com>
2023-11-13 06:13:30 -0700

    current header with icon is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,46 @@ var searchedArr = [];
 $("#search-btn").on("click",function() {
     var textStr = $("#search-inp").val();
     if (textStr.length > 0){
-        if (!searchedArr.includes(textStr)) {
-            searchedArr.push(textStr);
-            renderHistory(searchedArr);
-        }
+        var urlString = "https://api.openweathermap.org/data/2.5/weather?q=";
+        urlString += textStr;
+        urlString += "&APPID=0bf264ae251f7110c36368067c1f04d7&units=metric";
+        $.ajax({
+            url: urlString,
+            method: "GET",
+            dataType: "json",
+            success: function(data) {
+                console.log(data);
+                cityFound(textStr,searchedArr,data);
+            },
+            error: function(xhr, status, error) {
+                alert("City Not Found!");
+            }
+        })
     }
 })
 
+function cityFound(textStr,searchedArr,data) {
+    renderCurrentHeader(data);
+    if (!searchedArr.includes(textStr)) {
+        searchedArr.unshift(textStr);
+        renderHistory(searchedArr);
+    }
+}
+
 function cropHistory(searchedArr) {
     while (searchedArr.length > 10) {
-        searchedArr.shift();
+        searchedArr.pop();
     }
     return searchedArr;
+}
+
+function renderCurrentHeader(data) {
+    $("#current-city").text(data.name);
+    var todayDt = "(" + dayjs().format('M/D/YYYY') + ")";
+    $("#current-today").text(todayDt);
+    var icon = data.weather[0].icon;
+    iconUrl = `https://openweathermap.org/img/w/${icon}.png`;
+    $("#current-icon").attr("src",iconUrl);
 }
 
 function renderHistory(searchedArr) {
